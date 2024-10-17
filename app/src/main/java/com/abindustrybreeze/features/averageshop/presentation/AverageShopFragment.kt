efbac54f2050369637792918bba8d2fb511d7d48
Pujabasak <puja.basak@indusnet.co.in>
2024-10-17 10:39:47 +0530

    c

--- app/src/main/java/com/abindustrybreeze/features/averageshop/presentation/AverageShopFragment.kt
+++ app/src/main/java/com/abindustrybreeze/features/averageshop/presentation/AverageShopFragment.kt
@@ -755,7 +755,7 @@ class AverageShopFragment : BaseFragment(), DatePickerListener, View.OnClickList
         BaseActivity.isShopActivityUpdating = true
 /////////////
         revisitStatusList.clear()
-        for(i in 0..shopDataList?.size-1){
+        for(i in 0..shopDataList?.size!!-1){
             var data=AppDatabase.getDBInstance()?.shopVisitOrderStatusRemarksDao()!!.getSingleItem(shopDataList?.get(i)?.shop_revisit_uniqKey!!.toString())
             if(data!=null ){
                 var revisitStatusObj= ShopRevisitStatusRequestData()
@@ -811,7 +811,7 @@ class AverageShopFragment : BaseFragment(), DatePickerListener, View.OnClickList
                             if(!revisitStatusList.isEmpty()){
                                 callRevisitStatusUploadApi(revisitStatusList!!)
                             }
-                            for(i in 0..shopDataList?.size-1){
+                            for(i in 0..shopDataList?.size!!-1){
                                 callCompetetorImgUploadApi(shopDataList?.get(i)?.shop_id!!)
                             }
 
@@ -2918,7 +2918,7 @@ class AverageShopFragment : BaseFragment(), DatePickerListener, View.OnClickList
         ////////
         revisitStatusList.clear()
         var key:String = ""
-        for(i in 0..list_?.size-1){
+        for(i in 0..list_?.size!!-1){
             if(list_.get(i).shopid.equals(shopId)){
                 key=list_.get(i).shop_revisit_uniqKey!!.toString()
             }
